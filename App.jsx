0e453f008b93d9f662afc327fde85bb89b03bc5d
kmks43307-star <kmks43307@gmail.com>
2026-05-01 23:40:24 +0900

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+
+import { useEffect } from 'react'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+export default function App(){
+  useEffect(()=>{
+    const map = L.map('map').setView([35.6,139.7],10)
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map)
+    L.marker([35.6362,139.8593]).addTo(map).bindPopup("葛西臨海公園")
+  },[])
+  return <div id="map" style={{height:"100vh"}}></div>
+}
